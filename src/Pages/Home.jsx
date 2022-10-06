f7bbf288aedb884c0d71df5590ee8c15ae8e9baf
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-10-06 12:40:26 +0530

    hero section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Stack } from '@mui/material';
+import styled from '@emotion/styled';
+
+// components
+import Header from '../components/Header';
+import HeroSection from '../components/HeroSection';
+
+const HomeContainer = styled(Stack)({
+    width:'100%',
+    flexDirection:'column',
+})
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeContainer>
+        <Header/>
+        <HeroSection/>
+    </HomeContainer>
   )
 }
 
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+import heroImage from "../assets/icons/heroImage.svg";
+
+const HeroCallToAction = styled(Stack)({
+  maxWidth: "450px",
+  alignItems:'flex-start',
+  
+});
+
+const HeroImage = styled(Box)({
+});
+
+const ChallengeButton = styled(Button)({
+    backgroundColor:'white',
+    color:'black',
+    textTransform:'none',
+    marginTop:'2rem',
+    fontWeight:'bold',
+   "&:hover":{
+         backgroundColor:'black',
+         color:'white',
+    }
+    
+
+})
+
+const HeroSection = () => {
+  return (
+    <Stack
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-evenly"
+      width="100%"
+      sx={{color:'white',backgroundColor:'#003145'}}
+     
+    >
+      <HeroCallToAction>
+        <Typography
+          variant="h4"
+          fontWeight="600"
+          lineHeight="2.5rem"
+          fontFamily="Poppins"
+          marginBottom="1rem"
+        >
+          Accelerate Innovation with Global AI Challenges
+        </Typography>
+        <Typography>
+          AI Challenges at DPhi simulate real-world problems. It is a great
+          place to put your AI/Data Science skills to test on diverse datasets
+          allowing you to foster learning through competitions.
+        </Typography>
+        <ChallengeButton>
+            Create Challenge
+        </ChallengeButton>
+      </HeroCallToAction>
+      <HeroImage>
+        <img src={heroImage} />
+      </HeroImage>
+    </Stack>
+  );
+};
+
+export default HeroSection;
