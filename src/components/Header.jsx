64107086f15ac5dfee21b6d4cb953f50a3ea8424
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-10-06 12:30:56 +0530

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Stack } from '@mui/material';
+import styled from '@emotion/styled';
+import BrandLogo from "../assets/icons/brandLogo.png"
+
+const HeaderContainer = styled(Stack)({
+    width:'100%',
+    minHeight:'4rem',
+    justifyContent:'center',
+    backgroundColor:'white',
+    paddingLeft:'2rem',
+})
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <HeaderContainer>
+     <img
+      className='brandLogo'
+      src={BrandLogo}
+      height="40px"
+      width="40px"
+     />
+   </HeaderContainer>
   )
 }
 
-export default Header
+export default Header;
